62bb861b0d9edb1ec3b1eba8b33fbdfac9e23cbc
Yegor Bugayenko <yegor256@gmail.com>
2024-01-01 09:58:32 +0300

    #80 commands

--- lib/cobench/metrics/issues.rb
+++ lib/cobench/metrics/issues.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2023 Yegor Bugayenko
+# Copyright (c) 2022-2024 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -23,7 +23,7 @@ require_relative '../match'
 
 # Issues in GitHub API.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2022-2023 Yegor Bugayenko
+# Copyright:: Copyright (c) 2022-2024 Yegor Bugayenko
 # License:: MIT
 class Cobench::Issues
   def initialize(api, user, opts)
--- test/test_match.rb
+++ test/test_match.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2023 Yegor Bugayenko
+# Copyright (c) 2022-2024 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -24,7 +24,7 @@ require_relative '../lib/cobench/match'
 
 # Test for Match.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2022-2023 Yegor Bugayenko
+# Copyright:: Copyright (c) 2022-2024 Yegor Bugayenko
 # License:: MIT
 class TestMatch < Minitest::Test
   def test_positive
